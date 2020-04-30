420a2927eb878fd43045f477f4f9da5d8bcada8e
Hu Rui <hr101191@gmail.com>
2020-05-01 00:47:42 +0800

    Console Client Example Added

--- HelloWorld/OrleansHelloWorld/ConsoleClient/Program.cs
+++ HelloWorld/OrleansHelloWorld/ConsoleClient/Program.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 
-namespace SiloHost
+namespace ConsoleClient
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var isConsole = (!Debugger.IsAttached && args.Contains("--console"));
+            var isService = (!Debugger.IsAttached && args.Contains("--service"));
+            var isSystemd = (!Debugger.IsAttached && args.Contains("--systemd"));
+
+            //Run on console
+            if (isConsole)
+            {
+                CreateHostBuilder(args).UseConsoleLifetime().Build().Run();
+            }
+            //Run as a service on linux env
+            if (isSystemd)
+            {
+                CreateHostBuilder(args).UseSystemd().Build().Run();
+            }
+            //Run as windows service
+            if (isService)
+            {
+                CreateHostBuilder(args).UseWindowsService().Build().Run();
+            }
+            //Run on debugger
+            if (!isConsole && !isService && !isSystemd && Debugger.IsAttached)
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            else
+            {
+                Console.WriteLine("Invalid setting");
+                Environment.Exit(-1);
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    //Register any external config
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    //Register any local dependencies if required
+                    services.AddHostedService<Startup>(); //startup class template added here
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    //Register logger
+                });
         }
     }
 }
---
+++ HelloWorld/OrleansHelloWorld/ConsoleClient/Startup.cs
@@ -0,0 +1,52 @@
+﻿using GrainInterfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orleans;
+using Orleans.Configuration;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    public class Startup : IHostedService, IDisposable
+    {
+        public void Dispose()
+        {
+
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var client = CreateOrleansClient();
+            Console.WriteLine("Connected to Orleans cluster successfully...");
+            Console.WriteLine("Getting Grain IHelloWorld...");
+            var grain = client.GetGrain<IHelloWorld>(new Guid());
+            Console.WriteLine("Triggering method SayHello()...");
+            string result = await grain.SayHello();
+            Console.WriteLine("Result from grain:" + result);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private IClusterClient CreateOrleansClient()
+        {
+            var clientBuilder = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "ConsoleClient";
+                })
+                .ConfigureApplicationParts(parts => parts.AddFromApplicationBaseDirectory());
+
+            var client = clientBuilder.Build();
+
+            client.Connect().Wait();            
+            return client;
+        }
+    }
+}
