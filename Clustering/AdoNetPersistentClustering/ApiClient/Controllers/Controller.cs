66e9668681f7eef48ffb20e79bfb61511ec818d5
Hu Rui <hr101191@gmail.com>
2020-05-03 06:28:35 +0800

    WIP template - cont#

---
+++ Clustering/AdoNetPersistentClustering/ApiClient/Controllers/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using GoodbyeGrainInterface;
+using HelloGrainInterface;
+using Microsoft.AspNetCore.Mvc;
+using Orleans;
+
+namespace ApiClient.Controllers
+{
+    [Route("api")]
+    [ApiController]    
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly IClusterClient clusterClient;
+        private readonly IHello helloGrain;
+        private readonly IGoodbye goodbyeGrain;
+        public HelloWorldController(IClusterClient clusterClient)
+        {
+            this.clusterClient = clusterClient;
+            //Initialize the grains that the controller instance will use
+            helloGrain = this.clusterClient.GetGrain<IHello>(new Guid());
+            goodbyeGrain = this.clusterClient.GetGrain<IGoodbye>(new Guid());
+        }
+
+        [Route("hello")]
+        [HttpGet]
+        public async Task<string> Hello()
+        {
+            return await helloGrain.SayHello();
+        }
+
+        [Route("goodbye")]
+        [HttpGet]
+        public async Task<string> Goodbye()
+        {
+            return await goodbyeGrain.SayGoodbye();
+        }
+
+
+        [Route("hello-goodbye")]
+        [HttpGet]
+        public async Task<string> GoodbyeFromHelloGrain()
+        {
+            return await helloGrain.SayGoodbyeFromHelloGrain();
+        }
+    }
+}
